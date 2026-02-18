14a59ccd3c325405654163f5b7669b8dc3b5ff4a
Кира Монёв <kamonev@edu.hse.ru>
2026-02-18 23:41:13 +0300

    feat(UI): add pauseMenu and transition to main menu

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool isPaused = false;
+    [SerializeField] private GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+}
